072b76a5e674f203038fb428138b96e6fc6f581c
Complex and Sustainable Urban Networks (CSUN) Lab <csunlab@uic.edu>
2016-08-03 15:26:10 -0500

    Update GISF2E_qgis_OSM_v.1.00.py

--- QGIS/GISF2E_qgis_OSM_v.1.00.py
+++ QGIS/GISF2E_qgis_OSM_v.1.00.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-GISF2E python - v1.00
+GISF2E QGIS OSM - v1.00
 
 To cite, plese use: Karduni, A., Kermanshah, A., & Derrible, S., 2016 “A protocol to convert spatial polyline data to network formats and applications to world urban road networks”, Scientific Data, 3:160046
 The article is available at: http://www.nature.com/articles/sdata201646
