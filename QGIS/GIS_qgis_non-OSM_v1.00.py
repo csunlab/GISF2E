e9ddfe07236bdd8263671084cf80503aac2caaac
Complex and Sustainable Urban Networks (CSUN) Lab <csunlab@uic.edu>
2016-08-03 18:01:52 -0500

    First release of GISF2E QGIS python code for v1.00
    Note that the code was developed with pandas version 0.14.1 and it runs much faster for this version than with later versions.

--- QGIS/GIS_qgis_non-OSM_v1.00.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-GISF2E QGIS non-OSM - v1.00
-
-To cite, plese use: Karduni, A., Kermanshah, A., & Derrible, S., 2016 “A protocol to convert spatial polyline data to network formats and applications to world urban road networks”, Scientific Data, 3:160046
-The article is available at: http://www.nature.com/articles/sdata201646
-
-Main authors: Felipe Macena, Rodrigo Marinho
-"""
